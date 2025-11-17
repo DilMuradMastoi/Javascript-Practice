48a5b30f86b892e956ab37b64acc93ba5a66a094
DilMuradMastoi <webdeveloper1234567890003@gmail.com>
2025-11-17 23:21:19 +0500

    project completed

---
+++ string method js/app.js
@@ -0,0 +1,153 @@
+// console.log("hello world");
+
+// toLowerCase
+// toUpperCase
+// length
+// charAt
+// indexOf
+// slice
+// replace
+// replaceall
+// split
+
+
+// const fruits = ['apple' , 'banana' , 'orange' , 'watermelon' , 'guawa'];
+// console.log(fruits.slice(1 , 3 , "bababab" ))
+// console.log(fruits.indexOf('apple'));
+// console.log(fruits.includes('watermelon'));
+
+// const fruits = ['apple' , 'banana' , 'orange' , 'melon', 'gwava']
+// console.log(fruits.slice(1 , 4))
+// console.log(fruits.indexOf('apple'))
+// console.log(fruits.includes('gwava' ))
+
+// const username = 'Muhammad Abdullah';
+// console.log(username.toLocaleUpperCase())
+// console.log(username[3]);
+// console.log(username.length);
+// console.log(username.slice(3 , 7));
+
+// console.log(username.toLowerCase());
+// console.log(username.toUpperCase());
+
+
+// const username = 'Muhammad Abdullah'
+// console.log(username.length)
+// console.log(username.toLocaleLowerCase())
+// console.log(username.toLocaleUpperCase())
+// console.log(username.toLowerCase())
+
+// console.log(username[4])
+// console.log(username.slice(3 , 9))
+
+
+
+// const city = prompt('which city do you live?');
+
+// if(city.toLowerCase() === 'karachi'){
+//     console.log('you live in the safest city');
+// }else{
+//     console.log('zindagi jhand haa!');
+// }
+
+
+
+// const city = prompt("write your city name")
+
+// if(city.toUpperCase() === 'KARACHI'){
+//     console.log("good")
+// }
+// else{
+//     console.log("its not karaachi")
+// }
+
+
+
+
+
+
+// let message = "Ali and Sami are best and friends. They play cricket Ali and Sami and football Ali and Sami together.";
+
+// console.log(message[message.length - 7])
+// console.log(message.replace('Ali' , 'ahad'))
+// console.log(message.replaceAll('Ali' , "ahmes"))
+
+
+
+
+
+
+
+
+// const username = 'Muhammad Abdullah';
+// const Username = 'usman';
+// console.log(username[6]);
+// console.log(username.charAt(6));
+// console.log(username.indexOf('m'));
+// console.log(username.lastIndexOf('a'));
+// console.log(username.split('a'));
+// console.log(username.length - 4);
+// console.log(username[username.length - 3]);
+
+//  const username = 'MuhammadAbdullah';
+// console.log(username[9])
+// console.log(username.charAt(9))
+// console.log(username.indexOf('a'))
+// console.log(username.lastIndexOf('a'))
+// console.log(username.split('a'))
+// console.log(username.length - 9 ,)
+// console.log(username[username.length - 9])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//aik array banani ha cities ki. user input field ka undar koi bhi city ka naam likhaiga. Agar wo city us array ma mujood hogi screeen pa show hona chahyaa 'this city is the safest city' else 'this is not the safest city' 
+
+
+// const cities = ['karachi' , 'hyderabad' , 'larkana' , 'sukkar' , 'jamshoro' , 'shikarpur' , 'lahore' , 'gujrawala' , 'sahiwal' , 'peshawar' , 'quetta'];
+// const city = document.querySelector('#city')
+
+// function checkCity(){
+//     if (cities.includes(city.value)){
+//         console.log("safe")
+//     }
+//     else{
+//         console.log("not safe")
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://forms.gle/ot1ua7iUbUBEEKNA6
+
+
+
+
+
+
+
