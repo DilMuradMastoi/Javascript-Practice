26e9e237bd63bbc1f87c417650b4e31280791aeb
DilMuradMastoi <webdeveloper1234567890003@gmail.com>
2025-11-19 19:36:02 +0500

    project completed

---
+++ date and time/date.js
@@ -0,0 +1,132 @@
+// var
+// let
+// const
+
+// var username = "abdullah"
+//  username = "usman" // reassign
+//  var username = "Ammar" // redeclare
+
+// let age = 21
+// age = 22;   
+// let age = 22  // redeclare x
+
+// console.log(age)
+// console.log(username)
+
+
+
+// const city = "Quetta"
+// city = "karachi"  // reasign X
+
+// console.log(city)
+
+// let username;
+// username = "abdullah"
+
+// const username = "Abdullah";
+
+/////////////////////////////////////////////////////////
+// var username 
+// username = "abdullah"
+// username = "ahad"
+// var username = "aslam"
+
+// console.log(username);
+
+// let username 
+// username = "asfand"
+// username ="aamir"
+// username = "zubair"
+// let username = "sohail"  redeclare X
+
+// console.log(username)
+
+// const username = "ahmed"
+// console.log(username)
+////////////////////////////////////////////////////////////
+// let currentDate = new Date();
+// currentDate.setFullYear(2003)
+// currentDate.setDate(27)
+// console.log(currentDate);
+
+// let currentDate = new Date ()
+// currentDate.setFullYear(2007)
+// currentDate.setMonth(6)
+// currentDate.setDate(23)
+// currentDate.setHours(12)
+// currentDate.setMinutes(32)
+// console.log(currentDate)
+
+
+// let currentDate = new Date("July, 26 , 2025");
+// console.log(currentDate.getDay());
+// console.log(currentDate.getMonth());
+// console.log(currentDate.getFullYear());
+// console.log(currentDate.getHours());
+// console.log(currentDate.getMinutes());
+// console.log(currentDate.getSeconds());
+// console.log(currentDate.getMilliseconds());
+// console.log(typeof currentDate);
+// console.log(currentDate.toString());
+
+// console.log(currentDate.getTime());
+
+
+// let currentDate = new Date("July 21 , 2003")
+// console.log(currentDate.getDate());
+// console.log(currentDate.getDay());
+// console.log(currentDate.getFullYear());
+// console.log(currentDate.getMonth());
+// console.log(currentDate.getHours());
+// console.log(currentDate.getMinutes());
+// console.log(currentDate.getSeconds());
+// console.log(currentDate.getMilliseconds());
+// console.log(currentDate.getTime());
+// console.log(typeof currentDate);
+// console.log(currentDate.toString());
+////////////////////////////////////////////////////////
+// let currentDate = new Date("July 23 , 2007")
+
+// console.log(currentDate.getDate())
+// console.log(currentDate.getDay())
+// console.log(currentDate.getFullYear())
+// console.log(currentDate.getHours())
+// console.log(currentDate.getMilliseconds())
+// console.log(currentDate.getMinutes())
+// console.log(currentDate.getMonth())
+// console.log(currentDate.getSeconds())
+// console.log(currentDate.getTime())
+// console.log(currentDate.toString())
+// console.log(typeof currentDate)
+////////////////////////////////////////////////////////
+// function calculateAge(username , userBirth){
+//     let currentDate = new Date().getTime( );
+//     let userBirthDate = new Date(userBirth).getTime();
+
+//     let remainingMilliseconds = currentDate - userBirthDate;
+//     let userAgeInYear = remainingMilliseconds / 1000 / 60 / 60 / 24 / 365
+//     console.log(username + " age in years ==>" , Math.round(userAgeInYear));
+// }
+
+// calculateAge("Abdullah" , "November 27, 2003")
+// calculateAge("Aftab" , "August 4, 2004")
+// calculateAge("Rizwan" , "February 2, 2009")
+// calculateAge("Irfan" , "March 2, 1991")
+// calculateAge("Ubaidullah" , "January 1, 1970")
+// "November 27, 2003"
+
+
+
+
+
+// function calculateAge (username , userBirth){
+//     let currentDate = new Date().getTime();
+//     let userBirthDate = new Date(userBirth).getTime();
+
+//     let remainingMilliseconds = currentDate - userBirthDate ;
+//     let userAgeInYear = remainingMilliseconds / 1000 / 60 / 60 / 24 / 365 ;
+//     console.log(username + "Your Age In Years ==>" , Math.round(userAgeInYear))
+// }
+
+
+// calculateAge ("DilMurad" , "July 23 ,  2008")
